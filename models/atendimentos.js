b35878aebc670058623204519e418e4cca8415d3
Elias Moraes <eeliasmaia@gmail.com>
2021-09-17 10:55:05 -0300

    Finalizando API

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -70,13 +70,28 @@ class Atendimento {
     }
 
     altera(id, valores, res){
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
         const sql = 'UPDATE Atendimentos SET ? WHERE id = ?'
 
         conexao.query(sql, [valores, id], (erro, resultados) => {
             if(erro){
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res){
+        const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
